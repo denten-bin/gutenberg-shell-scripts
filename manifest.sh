b98356317f8d757e6ca1d2f7db8c7d1897964484
denten <denten@gmail.com>
2017-03-02 18:29:23 -0500

    add title parser to manifest

--- manifest.sh
+++ manifest.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
-# This script will iterate over all files in a directory to create a manifest
-# of documents using the gutenberg.org meta data fields. These include file
-# name, title, author, language, and encoding. The output is in .csv format
+# This script will iterate over all files in the current directory to create a
+# manifest of documents using the gutenberg.org meta data fields. These
+# include file name, title, author, language, and encoding. The output is in
+# .csv format
+
+# write first line to manifest file
+echo "file_name, title, author, language, encoding" > MANIFEST.csv
+
+# loop through all .txt files in folder
+for f in *.txt
+do
+
+    # file name
+    echo $f >> MANIFEST.csv
+
+    # add comma
+    echo ',' >> MANIFEST.csv
+
+    # append title
+    # Title: War and Peace
+    # Cut at the colon, return second term, cut space
+    grep "Title:" $f | cut -d':' -f2- | sed -e 's/ //' >> MANIFEST.csv
+
+done
+
+echo "MANIFEST.csv created in this directory"
