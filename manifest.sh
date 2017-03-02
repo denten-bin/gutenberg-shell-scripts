db2f7bad4c73625e08507caa4c9324b755b6e199
denten <denten@gmail.com>
2017-03-02 16:38:46 -0500

    add comments to the manifest

---
+++ manifest.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# This script will iterate over all files in a directory to create a manifest
+# of documents using the gutenberg.org meta data fields. These include file
+# name, title, author, language, and encoding. The output is in .csv format
