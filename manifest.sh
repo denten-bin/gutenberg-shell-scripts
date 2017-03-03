13fdbc71c4f1b74675c69d74c40e15192cb886b4
denten <denten@gmail.com>
2017-03-02 22:44:54 -0500

    finish manifest

--- manifest.sh
+++ manifest.sh
@@ -3,25 +3,33 @@
 # This script will iterate over all files in the current directory to create a
 # manifest of documents using the gutenberg.org meta data fields. These
 # include file name, title, author, language, and encoding. The output is in
-# .csv format
+# .csv format.
 
 # write first line to manifest file
-echo "file_name, title, author, language, encoding" > MANIFEST.csv
+echo "file_name, title, author, language" > MANIFEST.csv
 
 # loop through all .txt files in folder
 for f in *.txt
 do
 
-    # file name
-    echo $f >> MANIFEST.csv
-
-    # add comma
-    echo ',' >> MANIFEST.csv
+    # file name, -n for no carriage
+    echo -n $f', ' >> MANIFEST.csv
 
     # append title
     # Title: War and Peace
-    # Cut at the colon, return second term, cut space
-    grep "Title:" $f | cut -d':' -f2- | sed -e 's/ //' >> MANIFEST.csv
+    # Cut at the colon, return second term, cut initial space, remove punct & carriage
+    title=$(grep "Title:" $f | cut -d':' -f2- | sed -e 's/ //' | tr -d \
+    "[:punct:]" | tr -d '\r' )
+
+    author=$(grep "Author:" $f | cut -d':' -f2- | sed -e 's/ //' | tr -d \
+    "[:punct:]" | tr -d '\r' )
+
+    lang=$(grep "Language:" $f | cut -d':' -f2- | sed -e 's/ //' | tr -d \
+    "[:punct:]" | tr -d '\r' )
+
+    echo -n $title', ' >> MANIFEST.csv
+    echo -n $author', ' >> MANIFEST.csv
+    echo $lang >> MANIFEST.csv
 
 done
 
